cc2ca52d6df5cab3c5e03c6a6a81fb51634fa0bd
Chip Cullen <ccullen@pbs.org>
2020-11-25 23:37:59 -0500

    more tests

--- src/utils/toNamed.test.ts
+++ src/utils/toNamed.test.ts
@@ -15,3 +15,19 @@ describe('rgbToNamed', () => {
     expect(rgbToNamed([255, 0, 254])).toBe('');
   });
 });
+
+describe('rgbaToNamed', () => {
+  it('should return names for matching rgba', () => {
+    expect(rgbaToNamed([255, 255, 255, 1])).toBe('White');
+    expect(rgbaToNamed([0, 0, 0, 1])).toBe('Black');
+    expect(rgbaToNamed([255, 0, 0, 1])).toBe('Red');
+    expect(rgbaToNamed([255, 0, 255, 1])).toBe('Fuchsia');
+  });
+
+    it('should return empty string for anything else', () => {
+    expect(rgbaToNamed([255, 255, 254, 1])).toBe('');
+    expect(rgbaToNamed([0, 0, 1, 1])).toBe('');
+    expect(rgbaToNamed([255, 0, 0, 0])).toBe('');
+    expect(rgbaToNamed([255, 0, 255, 0.99])).toBe('');
+  });
+});

ede4871831aae509daab2372edc1eba9061a1b6a
Chip Cullen <ccullen@pbs.org>
2020-11-27 09:51:45 -0500

    to hsl tests

--- src/utils/toHsl.test.ts
+++ src/utils/toHsl.test.ts
@@ -2,6 +2,20 @@ import { rgbToHsl, hex6ToHsl, hslToHsl } from './toHsl';
 
 describe('hslToHsl', () => {
   it('should return correct value from HSL', () => {
-    expect(hslToHsl(`hsl(120, 50%, 50%)`)).toMatchObject([120, 50, 50])
+    expect(hslToHsl(`hsl(120, 50%, 50%)`)).toMatchObject([120, 50, 50]);
+  });
+});
+
+describe('rgbToHsl', () => {
+  it('should return correct value from RGB', () => {
+    expect(rgbToHsl(`rgb(255, 255, 255)`)).toMatchObject([0, 0, 100]);
+    expect(rgbToHsl(`rgb(0, 0, 0)`)).toMatchObject([0, 0, 0]);
+  });
+});
+
+describe('hex6ToHsl', () => {
+  it('should return correct value from Hex6', () => {
+    expect(hex6ToHsl(`#fff`)).toMatchObject([0, 0, 100]);
+    expect(hex6ToHsl(`#000000`)).toMatchObject([0, 0, 0]);
   });
 });

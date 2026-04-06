88440c43d908e65a955dde1f41a0cf4827f553e7
Chip Cullen <ccullen@pbs.org>
2026-04-06 11:57:18 -0400

    more updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: ['./src/setupTests.ts'],
   },
 })
